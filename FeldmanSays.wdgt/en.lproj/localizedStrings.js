3712a32f45ad99562ff29e7cfce8e69b2aea55e2
Brian James <brianb.james@mac.com>
2014-07-21 23:37:19 -0700

    PULLING OLD CODE

--- FeldmanSays.wdgt/en.lproj/localizedStrings.js
+++
@@ -1,6 +0,0 @@
-var localizedStrings = new Object;
-
-localizedStrings["Hello, World!"] = "Hello, World!";
-localizedStrings["Done"] = "Done";
-localizedStrings["Feldman Says"] = "Feldman Says";
-
--- OutOfTheWhitehouse.wdgt/en.lproj/localizedStrings.js
+++
@@ -1,9 +0,0 @@
-var localizedStrings = new Object;
-
-localizedStrings["Countdown to"] = "Countdown to %s";
-localizedStrings["Done"] = "Done";
-
-localizedStrings["DAYS"]  = "DAYS";
-localizedStrings["HOURS"] = "HOURS";
-localizedStrings["MIN"]   = "MIN";
-localizedStrings["SEC"]   = "SEC";
